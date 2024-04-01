e6f0f683a32ed68c81ccbdc6670f72aa9b3e072e
Francisco Carmona <jcarmfran@gmail.com>
2024-04-01 07:18:00 -0500

    Change formatting

--- src/textSummarizer/pipeline/stage_04_model_trainer.py
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -6,8 +6,8 @@ from textSummarizer.logging import logger
 class ModelTrainerPipeline:
     def __init__(self):
         pass
-    
-    
+
+
     def main(self):
         config = ConfigurationManager()
         model_trainer_config = config.get_model_trainer_config()
